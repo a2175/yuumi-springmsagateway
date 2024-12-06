67c141cf96aaf623d4705a84592a12c961259faa
a2175 <as217512@gmail.com>
2024-12-06 16:26:45 +0900

    init

---
+++ src/main/java/hello/springmsagateway/SpringmsagatewayApplication.java
@@ -0,0 +1,13 @@
+package hello.springmsagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmsagatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmsagatewayApplication.class, args);
+	}
+
+}
